7b2e3d4ed57189952f21060285aa886aef7480c9
Juan Carlos Avila <juanvs23@gmail.com>
2024-04-17 20:36:24 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": "./src",
     },
   },
 })
