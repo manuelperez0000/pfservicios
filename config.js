695b9979c24ccd492097efc18eafefb34b88bad9
manuel perez <manuelperez.0000@gmail.com>
2024-05-07 15:50:15 -0400

    otro mas

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
     telephone: 584141220527,
     siteProdUrl:'https://back-pf-fork.vercel.app',
     siteDevUrl:'http://localhost:5173',
-    devServerUrl:'http://localhost:8000',
+    devServerUrl:'https://back-pf-fork.vercel.app',
     prodServerUrl:'https://back-pf-fork.vercel.app',
     secretCode: 'pfservicios123',
 }
